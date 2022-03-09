e4dbaa053f2622429944c11e26881d5c9ac464c0
Sam Svindland <samsvindland@Sams-MacBook-Pro.local>
2022-03-09 17:51:38 -0500

    fix eslint errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,70 +16,68 @@ interface IPref {
     start: number
 }
 
-// eslint-disable-next-line max-len
 export const sendNotificationsHttp = functions.https.onRequest(async (req, res) => {
-  await sendNotifications();
+    await sendNotifications();
 
-  res.end();
+    res.end();
 });
 
 
-// eslint-disable-next-line max-len
-export const sendNotificationsScheduler = functions.pubsub.schedule('0 * * * *').onRun(async (context) => {
-  await sendNotifications();
+export const sendNotificationsScheduler = functions.pubsub.schedule('0 * * * *').onRun(async () => {
+    await sendNotifications();
 
-  return null;
+    return null;
 });
 
 const sendNotifications = async () => {
-  const tokens: IToken[] = [];
-  const prefs: IPref[] = [];
+    const tokens: IToken[] = [];
+    const prefs: IPref[] = [];
 
-  await db.collection('tokens').get().then((snapshot) => {
-    snapshot.forEach((document) => {
-      tokens.push({
-        uid: document.id,
-        token: document.get('token'),
-      });
+    await db.collection('tokens').get().then((snapshot) => {
+        snapshot.forEach((document) => {
+            tokens.push({
+                uid: document.id,
+                token: document.get('token'),
+            });
+        });
     });
-  });
 
-  await db.collection('preferences').get().then((snapshot) => {
-    snapshot.forEach((document) => {
-      prefs.push({
-        uid: document.id,
-        start: document.get('start')?.toDate(),
-      });
+    await db.collection('preferences').get().then((snapshot) => {
+        snapshot.forEach((document) => {
+            prefs.push({
+                uid: document.id,
+                start: document.get('start')?.toDate(),
+            });
+        });
     });
-  });
 
-  tokens.forEach((token) => {
-    prefs.forEach((pref) => {
-      if (token.uid === pref.uid) {
-        const current = moment(`2000 1 1 ${(new Date).getHours()}:00:00`);
-        const start = (new Date(pref.start));
-        let payload: admin.messaging.MessagingPayload | undefined = undefined;
+    tokens.forEach((token) => {
+        prefs.forEach((pref) => {
+            if (token.uid === pref.uid) {
+                const current = moment(`2000 1 1 ${(new Date).getHours()}:00:00`);
+                const start = (new Date(pref.start));
+                let payload: admin.messaging.MessagingPayload | undefined = undefined;
 
-        if (start.getHours() === current.hour() && moment().isoWeekday() === 7) {
-          payload = {
-            notification: {
-              title: 'Good Morning!',
-              body: 'Remember to weigh and measure yourself',
-            },
-          };
-        } else if (start.getHours() === current.hour()) {
-          payload = {
-            notification: {
-              title: 'Good Morning!',
-              body: 'Remember to weigh yourself',
-            },
-          };
-        }
+                if (start.getHours() === current.hour() && moment().isoWeekday() === 7) {
+                    payload = {
+                        notification: {
+                            title: 'Good Morning!',
+                            body: 'Remember to weigh and measure yourself',
+                        },
+                    };
+                } else if (start.getHours() === current.hour()) {
+                    payload = {
+                        notification: {
+                            title: 'Good Morning!',
+                            body: 'Remember to weigh yourself',
+                        },
+                    };
+                }
 
-        if (payload) {
-          fcm.sendToDevice(token.token, payload);
-        }
-      }
+                if (payload) {
+                    fcm.sendToDevice(token.token, payload);
+                }
+            }
+        });
     });
-  });
 };
