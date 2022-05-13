3e6ad314453ad03ae95c479164ccd3c154874ce2
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-05-13 11:36:11 -0400

    update flutter and dependencies

--- lib/layouts/about.dart
+++ lib/layouts/about.dart
@@ -40,7 +40,7 @@ class AboutPage extends StatelessWidget {
                 style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               Text(
-                '1.0.2',
+                '1.2.1',
                 style: TextStyle(color: Colors.white, fontSize: 12),
               ),
             ],
