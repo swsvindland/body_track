3b64a6babfd89959b905ac3a9b22626d667346f2
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-08-03 12:17:59 -0400

    target android 33

--- lib/layouts/about.dart
+++ lib/layouts/about.dart
@@ -41,7 +41,7 @@ class AboutPage extends StatelessWidget {
                 style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               Text(
-                '1.6.4',
+                '1.6.5',
                 style: TextStyle(color: Colors.white, fontSize: 12),
               ),
             ],
